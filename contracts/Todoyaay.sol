8f4c821d73b1883057140b63362285ba710dad94
anay-roy <anayroy42003@gmail.com>
2023-04-20 15:02:41 +0530

    initial commit

--- contracts/Todoyaay.sol
+++ contracts/Todoyaay.sol
@@ -10,10 +10,42 @@ contract Todoyaay {
     string[] public message;
     uint256[] public messageID;
 
-    struct todoListApp {
+    struct ToDoListApp {
         address account;
         uint256 userID;
         string message;
         bool completed;
+    }
+
+    event ToDoevent(
+        address indexed account,
+        uint256 indexed userID,
+        string message,
+        bool completed
+    );
+
+    mapping(address => ToDoListApp) public toDoListApp;
+
+    constructor() {
+        ownerOfContract = msg.sender;
+    }
+
+    function inc() internal {
+        _idUser++;
+    }
+
+    function createList(string calldata _message) external {
+        inc();
+        uint256 idNumber = _idUser;
+        ToDoListApp storage toDo = toDoListApp[msg.sender];
+
+        toDo.account = msg.sender;
+        toDo.message = _message;
+        toDo.completed = false;
+        toDo.userID = idNumber;
+
+        creators.push(msg.sender);
+        message.push(_message);
+        messageID.push(idNumber);
     }
 }
